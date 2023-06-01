e51ebaf56b4556571c7aa8fc8eaea046ed6cd61e
Nil Borodulia <nil.b@oxtech.org>
2023-06-01 11:15:15 +0300

    change test

--- app/task/shell_runner_test.go
+++ app/task/shell_runner_test.go
@@ -8,22 +8,37 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/google/uuid"
+
+	store "github.com/umputun/updater/app/store"
 )
 
-func TestShellRunner_Run(t *testing.T) {
-	sr := ShellRunner{}
+func getStore() store.Store {
+    sec := store.Store {
+        StorePath: "/var/tmp/jtrw_updater_test.db",
+    }
 
+    sec.JBolt = sec.NewStore()
+
+    return sec
+}
+
+func TestShellRunner_Run(t *testing.T) {
+    sec := getStore()
+	sr := ShellRunner{DataStore: sec}
+	uuidStr := uuid.New().String()
 	{
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "echo 123", lw)
+		err := sr.Run(context.Background(), "echo 123", lw, uuidStr)
 		t.Log(lw.String())
 		require.NoError(t, err)
 		assert.Equal(t, "123\n", lw.String())
 	}
 
 	{
+	    uuidStr = uuid.New().String()
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "no-such-command 123", lw)
+		err := sr.Run(context.Background(), "no-such-command 123", lw, uuidStr)
 		require.Error(t, err)
 		t.Log(lw.String())
 		assert.Contains(t, lw.String(), "not found")
@@ -31,7 +46,7 @@ func TestShellRunner_Run(t *testing.T) {
 
 	{
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "@no-such-command 123", lw)
+		err := sr.Run(context.Background(), "@no-such-command 123", lw, uuidStr)
 		t.Log(lw.String())
 		require.NoError(t, err)
 		assert.Contains(t, lw.String(), "not found")
@@ -40,43 +55,48 @@ func TestShellRunner_Run(t *testing.T) {
 }
 
 func TestShellRunner_RunMultiLine(t *testing.T) {
-	sr := ShellRunner{}
-
+    sec := getStore()
+	sr := ShellRunner{DataStore: sec}
+    uuidStr := uuid.New().String()
 	{
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "echo 123\necho 567\n", lw)
+		err := sr.Run(context.Background(), "echo 123\necho 567\n", lw, uuidStr)
 		require.NoError(t, err)
 		assert.Equal(t, "123\n567\n", lw.String())
 	}
 
 	{
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "echo 123\nno-such-command 123", lw)
+		err := sr.Run(context.Background(), "echo 123\nno-such-command 123", lw, uuidStr)
 		require.Error(t, err)
 		assert.Contains(t, lw.String(), "not found")
 	}
 
 	{
 		lw := bytes.NewBuffer(nil)
-		err := sr.Run(context.Background(), "echo 123\n@no-such-command 123", lw)
+		err := sr.Run(context.Background(), "echo 123\n@no-such-command 123", lw, uuidStr)
 		require.NoError(t, err)
 		assert.Contains(t, lw.String(), "not found")
 	}
 
 }
 func TestShellRunner_RunBatch(t *testing.T) {
-	sr := ShellRunner{BatchMode: true, TimeOut: time.Second}
+    sec := getStore()
+	sr := ShellRunner{BatchMode: true, TimeOut: time.Second, DataStore: sec}
 	lw := bytes.NewBuffer(nil)
-	err := sr.Run(context.Background(), "echo 123\necho 345", lw)
+	uuidStr := uuid.New().String()
+	err := sr.Run(context.Background(), "echo 123\necho 345", lw, uuidStr)
 	require.NoError(t, err)
 	assert.Equal(t, "123\n345\n", lw.String())
 }
 
 func TestShellRunner_RunBatchTimeOut(t *testing.T) {
-	sr := ShellRunner{BatchMode: true, TimeOut: time.Millisecond * 100}
+    sec := getStore()
+	sr := ShellRunner{BatchMode: true, TimeOut: time.Millisecond * 100, DataStore: sec}
 	lw := bytes.NewBuffer(nil)
 	st := time.Now()
-	err := sr.Run(context.Background(), "sleep 1 && sleep 1 && echo 123\necho 345", lw)
+	uuidStr := uuid.New().String()
+	err := sr.Run(context.Background(), "sleep 1 && sleep 1 && echo 123\necho 345", lw, uuidStr)
 	require.Error(t, err)
 	assert.True(t, time.Since(st) < time.Second*2)
 }
